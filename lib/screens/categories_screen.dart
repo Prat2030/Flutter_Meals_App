a055d694095dfa584412f12dd7eb2bb2034ecab1
Prat2030 <pratyushjindal2003@gmail.com>
2022-06-03 22:02:25 +0530

    organised our widgets into screens and added info about meal below image using dummy_data

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, unused_import, invalid_required_positional_param
+
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+import '../widgets/category_item.dart';
+import '../dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meal Categories'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                  // catData.title,
+                  // catData.color,
+                  id: catData.id,
+                  title: catData.title, // converted to named argument
+                  color: catData.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import '../screens/category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+
+  const CategoryItem({
+    required this.id,
+    required this.title,
+    required this.color,
+  });
+
+  void _selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(CategoryMealsScreen.routeName, arguments: {
+      'id': id,
+      'title': title,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      hoverColor: Colors.pink,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.subtitle1,
+          textAlign: TextAlign.center,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
